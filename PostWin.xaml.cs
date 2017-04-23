c2d6fde638f55eda1d01cff37b6f8d2c3bd5dffb
Nisefal <kolyabogdanenko@gmail.com>
2017-04-23 13:52:28 +0300

    New files

---
+++ PostWin.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Version_1
+{
+    /// <summary>
+    /// Interaction logic for PostWin.xaml
+    /// </summary>
+    public partial class PostWin : Window
+    {
+        public PostWin()
+        {
+            InitializeComponent();
+        }
+
+
+
+
+        ////////////////////////////////
+        ///   BUTTON_CLICK_SECTION   ///
+        ////////////////////////////////
+
+
+
+        ////////////////////////////////
+        ///   BUTTON_ENTER_SECTION   ///
+        ////////////////////////////////
+
+
+
+        /////////////////////////////
+        ///   FUNCTIONS_SECTION   ///
+        /////////////////////////////
+
+
+
+    }
+}
