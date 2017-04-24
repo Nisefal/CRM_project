6f267c9aa8551e1bd19333933f841cc65ceef734
Nisefal <kolyabogdanenko@gmail.com>
2017-04-24 19:43:56 +0300

    Version 1
    CW+lab1(complete)

---
+++ Proposition.xaml.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace Version_1
+{
+    /// <summary>
+    /// Interaction logic for Proposition.xaml
+    /// </summary>
+    public partial class Proposition : Window
+    {
+        string CM = "Коментар";
+        public Proposition()
+        {
+            InitializeComponent();
+        }
+
+
+        ////////////////////////////////
+        ///   FOCUS_SECTION   /////////////////////////////////////////////////////////////////////////////
+        ////////////////////////////////
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Comm_FocusableChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+
+        }
+
+        private void Comm_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (Comm.Text == CM)
+                Comm.Text = "";
+
+        }
+
+        private void Comm_LostFocus(object sender, RoutedEventArgs e)
+        {
+                if (Comm.Text == "")
+                    Comm.Text = CM;
+        }
+
+
+        ////////////////////////////////
+        ///   BUTTON_CLICK_SECTION   /////////////////////////////////////////////////////////////////////////////
+        ////////////////////////////////
+
+
+
+        ////////////////////////////////
+        ///   BUTTON_ENTER_SECTION   /////////////////////////////////////////////////////////////////////////////
+        ////////////////////////////////
+
+
+
+        /////////////////////////////
+        ///   FUNCTIONS_SECTION   /////////////////////////////////////////////////////////////////////////////
+        /////////////////////////////
+
+
+
+
+        ////////////////////////////////
+        ///   MENU_INITS+FUNCTIONS   /////////////////////////////////////////////////////////////////////////////
+        ////////////////////////////////
+
+        private void StudItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Sett_Click(object sender, RoutedEventArgs e)
+        {
+            Settings modalWindow = new Settings();
+            modalWindow.ShowDialog();
+        }
+
+        private void EmissItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+        //////////////////////////////
+        ///   ALL_INITIALIZATION   /////////////////////////////////////////////////////////////////////////////
+        //////////////////////////////
+        private void InitPics()
+        {
+            try // icon&background&cursor
+            {
+                Uri iconUri = new Uri("./Images/Icon.ico", UriKind.RelativeOrAbsolute);
+                this.Icon = BitmapFrame.Create(iconUri);
+                ImageBrush myBrush = new ImageBrush();
+                myBrush.ImageSource = new BitmapImage(new Uri("./Images/Village.jpg", UriKind.Relative));
+                this.Background = myBrush;
+                this.Cursor = new Cursor(Directory.GetCurrentDirectory() + "@/./Images/Pointer_hand.cur");
+            }
+            catch (System.IO.DirectoryNotFoundException)
+            {
+                try
+                {
+                    Uri iconUri = new Uri("../../Images/Icon.ico", UriKind.RelativeOrAbsolute);
+                    this.Icon = BitmapFrame.Create(iconUri);
+                    ImageBrush myBrush = new ImageBrush();
+                    myBrush.ImageSource = new BitmapImage(new Uri("../../Images/Village.jpg", UriKind.Relative));
+                    this.Background = myBrush;
+                    this.Cursor = new Cursor(Directory.GetCurrentDirectory() + "@/../../Images/Pointer_hand.cur");
+                }
+                catch (DirectoryNotFoundException)
+                {
+                    Uri iconUri = new Uri("../Images/Icon.ico", UriKind.RelativeOrAbsolute);
+                    this.Icon = BitmapFrame.Create(iconUri);
+                    ImageBrush myBrush = new ImageBrush();
+                    myBrush.ImageSource = new BitmapImage(new Uri("../Images/Village.jpg", UriKind.Relative));
+                    this.Background = myBrush;
+                    this.Cursor = new Cursor(Directory.GetCurrentDirectory() + "@/../Images/Pointer_hand.cur");
+                }
+            }
+        }
+    }
+}
---
+++ WarnWin.xaml.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace Version_1
+{
+    /// <summary>
+    /// Interaction logic for WarnWin.xaml
+    /// </summary>
+    public partial class WarnWin : Window
+    {
+        public WarnWin()
+        {
+            InitializeComponent();
+        }
+
+        ////////////////////////////////
+        ///   BUTTON_CLICK_SECTION   /////////////////////////////////////////////////////////////////////////////
+        ////////////////////////////////
+
+
+
+        ////////////////////////////////
+        ///   BUTTON_ENTER_SECTION   /////////////////////////////////////////////////////////////////////////////
+        ////////////////////////////////
+
+
+
+        /////////////////////////////
+        ///   FUNCTIONS_SECTION   /////////////////////////////////////////////////////////////////////////////
+        /////////////////////////////
+
+
+
+
+        ////////////////////////////////
+        ///   MENU_INITS+FUNCTIONS   /////////////////////////////////////////////////////////////////////////////
+        ////////////////////////////////
+
+        private void StudItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Sett_Click(object sender, RoutedEventArgs e)
+        {
+            Settings modalWindow = new Settings();
+            modalWindow.ShowDialog();
+        }
+
+        private void EmissItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+        //////////////////////////////
+        ///   ALL_INITIALIZATION   /////////////////////////////////////////////////////////////////////////////
+        //////////////////////////////
+        private void InitPics()
+        {
+            try // icon&background&cursor
+            {
+                Uri iconUri = new Uri("./Images/Icon.ico", UriKind.RelativeOrAbsolute);
+                this.Icon = BitmapFrame.Create(iconUri);
+                ImageBrush myBrush = new ImageBrush();
+                myBrush.ImageSource = new BitmapImage(new Uri("./Images/Village.jpg", UriKind.Relative));
+                this.Background = myBrush;
+                this.Cursor = new Cursor(Directory.GetCurrentDirectory() + "@/./Images/Pointer_hand.cur");
+            }
+            catch (System.IO.DirectoryNotFoundException)
+            {
+                try
+                {
+                    Uri iconUri = new Uri("../../Images/Icon.ico", UriKind.RelativeOrAbsolute);
+                    this.Icon = BitmapFrame.Create(iconUri);
+                    ImageBrush myBrush = new ImageBrush();
+                    myBrush.ImageSource = new BitmapImage(new Uri("../../Images/Village.jpg", UriKind.Relative));
+                    this.Background = myBrush;
+                    this.Cursor = new Cursor(Directory.GetCurrentDirectory() + "@/../../Images/Pointer_hand.cur");
+                }
+                catch (DirectoryNotFoundException)
+                {
+                    Uri iconUri = new Uri("../Images/Icon.ico", UriKind.RelativeOrAbsolute);
+                    this.Icon = BitmapFrame.Create(iconUri);
+                    ImageBrush myBrush = new ImageBrush();
+                    myBrush.ImageSource = new BitmapImage(new Uri("../Images/Village.jpg", UriKind.Relative));
+                    this.Background = myBrush;
+                    this.Cursor = new Cursor(Directory.GetCurrentDirectory() + "@/../Images/Pointer_hand.cur");
+                }
+            }
+        }
+    }
+}
